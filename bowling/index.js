88e9ce7d626454de10341a2c55d1d29f9bd8b5a8
angela <angela@aviak.ca>
2020-08-07 07:00:39 -0600

    add not a number tests of roll input

--- bowling/index.js
+++ bowling/index.js
@@ -22,7 +22,7 @@ class Game {
     return totalscore;
   }
 
-  catchrollerrors(pins) {
+  checkroll(pins) {
     x = pins;
     try {
       if (x == '') throw "need a number for your roll of 0 to 10 pins";
@@ -35,59 +35,58 @@ class Game {
       console.log("Input is " + err);
       x = -1;
     }
-
+    // TODO: check for if 2nd roll would be more than 10
+    
     return(x);
   }
 
   roll(pins) {
-    numpins = 
-    if (this.newframe == true) {
-      let newframe = {
-        frame: this.index+1,
-        roll1: pins,
-        roll2: 0,
-        bonus: 0,
-      }
-      //deal with spare bonus
-      if (this.bonus == 'spare') {
-        this.frames[this.index-1].bonus = pins;
-        this.bonus = '';
-      }
-      // deal with case of 2 strikes in a row bonus
-      if (this.bonus == 'strike') {
-        this.frames[this.index-1].bonus = pins;
-        // deal with case of 3 strikes in a row bonus
-        if (this.index > 1) {
-          if (this.frames[this.index-2].roll1 == 10) {
-            this.frames[this.index-2].bonus+=10;
+    pinnum = checkroll(pins);
+    if (pinnum != -1) {
+      if (this.newframe == true) {
+        let newframe = {
+          frame: this.index+1,
+          roll1: pins,
+          roll2: 0,
+          bonus: 0,
+        }
+        //deal with spare bonus
+        if (this.bonus == 'spare') {
+          this.frames[this.index-1].bonus = pins;
+          this.bonus = '';
+        }
+        // deal with case of 2 strikes in a row bonus
+        if (this.bonus == 'strike') {
+          this.frames[this.index-1].bonus = pins;
+          // deal with case of 3 strikes in a row bonus
+          if (this.index > 1) {
+            if (this.frames[this.index-2].roll1 == 10) {
+              this.frames[this.index-2].bonus+=10;
+            }
           }
         }
-      }
-      
-      if (pins == 10) {
-        this.bonus = 'strike';
-        this.index++;
+        if (pins == 10) {
+          this.bonus = 'strike';
+          this.index++;
+        }
+        else {
+          this.newframe = false;
+        }
+        this.frames.push(newframe);
       }
       else {
-        this.newframe = false;
+        this.frames[this.index].roll2 = pins;
+        //deal with 2nd ball bonus after strike
+        if (this.bonus == 'strike') {
+          this.frames[this.index-1].bonus+=pins;
+          this.bonus = '';
+        }
+        this.newframe = true;
+        if (this.frames[this.index].roll1 + this.frames[this.index].roll2 == 10)
+          this.bonus = 'spare';
+        this.index++;
       }
-      this.frames.push(newframe);
-      
     }
-    else {
-      this.frames[this.index].roll2 = pins;
-      //deal with 2nd ball bonus after strike
-      if (this.bonus == 'strike') {
-        this.frames[this.index-1].bonus+=pins;
-        this.bonus = '';
-      }
-      this.newframe = true;
-      if (this.frames[this.index].roll1 + this.frames[this.index].roll2 == 10)
-        this.bonus = 'spare';
-      this.index++;
-      
-    }
-
   }
 }
 
